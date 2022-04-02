fb1325d92fac82e2c58bd710fb81430e66ae0753
Shawn Lee <changoloco.evg@gmail.com>
2022-04-02 08:16:49 -0600

    refactor: set base font size to 15px

--- src/App.svelte
+++ src/App.svelte
@@ -11,10 +11,16 @@
       <h1 class="text-2xl font-bold text-slate-700">
         Improve your front-end skills by building projects
       </h1>
-      <p class="p-2 | text-[15px]">
+      <p class="p-2">
         Scan the QR code to visit Frontend Mentor and take your coding skills to the next
         level
       </p>
     </div>
   </div>
 </main>
+
+<style>
+  :global(html) {
+    font-size: 15px;
+  }
+</style>
